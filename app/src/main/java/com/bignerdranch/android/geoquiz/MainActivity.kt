3c1218d90d1cfa3dcf6e175e136c679942379a79
Cameron Larsen <camjlarsen@gmail.com>
2023-09-07 16:45:44 -0700

    feat: Disable answer buttons after selection

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.bignerdranch.android.geoquiz
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.bignerdranch.android.geoquiz.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,10 +22,13 @@ class MainActivity : AppCompatActivity() {
         Question(R.string.question_asia, true),
     )
 
+    private var resultBank = BooleanArray(questionBank.size)
+
     private var currentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle?) called")
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -37,9 +43,40 @@ class MainActivity : AppCompatActivity() {
         binding.nextButton.setOnClickListener {
             currentIndex = (currentIndex + 1) % questionBank.size
             updateQuestionText()
+            updateButtons(false)
+        }
+
+        binding.prevButton.setOnClickListener{
+            currentIndex = (currentIndex - 1) % questionBank.size
+            updateQuestionText()
         }
 
         updateQuestionText()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
     }
 
     private fun updateQuestionText() {
@@ -49,6 +86,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun checkAnswer(userAnswer: Boolean, view: View) {
         val correctAnswer = questionBank[currentIndex].answer
+        resultBank[currentIndex] = correctAnswer
 
         val messageResId = if (userAnswer == correctAnswer) {
             R.string.correct_toast
@@ -57,6 +95,11 @@ class MainActivity : AppCompatActivity() {
         }
 
         Snackbar.make(view, messageResId, Snackbar.LENGTH_SHORT).show()
+        updateButtons(false)
+    }
 
+    private fun updateButtons(value: Boolean){
+        binding.trueButton.isEnabled = value
+        binding.falseButton.isEnabled = value
     }
 }
